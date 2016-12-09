14c8196ba6ec330cc5c3f804626be417a755adbf
Muhammad Avicenna Naradipa <cennanaradipa@Cennas-Mac.local>
2016-12-09 12:02:53 +0800

    created half of algo

--- work/Cenna/HOML_clustering_cenna.py
+++ work/Cenna/HOML_clustering_cenna.py
@@ -1,20 +1,20 @@
 import numpy as np
+from sklearn.metrics.pairwise import cosine_similarity
 
 class HOMLClusterAlgo(object):
-	"""
-		This is a template for a clustering model class.
-		Build the functions required.
-	"""
-	"""
-		initialize 1st cluster (put first vector into cluster)
-		update aux cluster
+		# This is a template for a clustering model class.
+		# Build the functions required.
 
-		do rest of the samples
-			do for all aux cluster
-				check distance and cosine values
-				assign a value that shows the fitting of the cluster
-				if pass threshold # modify for solution 1
-					mark this cluster eligible
+
+		# initialize 1st cluster (put first vector into cluster)
+		# update aux cluster
+
+		# do rest of the samples
+		# 	do for all aux cluster
+				# check distance and cosine values
+				# assign a value that shows the fitting of the cluster
+				# if pass threshold # modify for solution 1
+					# mark this cluster eligible
 
 			if eligible cluster is = 0
 				create new cluster
@@ -58,25 +58,69 @@ class HOMLClusterAlgo(object):
 
 
 
-	"""
+
 
 	def __init__(self, *args, **kwargs):
-		self.k_clusters = k_clusters # Remove if your algo discovers k
-		self.cluster_labels = None
+		self.cluster_labels = None			# cluster labels
+		self.aux_cluster = aux_cluster 		# average of cluster member
+		self.sim_score = sim_score			# similarity score for comparison
+		self.eligibility = eligibility		# eligibility statement
 		pass
 
 	def fit(self, feature_vectors):
-		"""
-			This method receives the objects to cluster
-			and finds the assigned cluster labels.
-
-			--- Explain any function arguments ---
-
-		"""
+		# """
+		# 	This method receives the objects to cluster
+		# 	and finds the assigned cluster labels.
+		#
+		# 	--- Explain any function arguments ---
+		#
+		# """
 
 		# Typecheck the input data
 		if not isinstance(feature_vectors, (np.ndarray, list)):
 			raise TypeError("Feature vectors not numpy arrays.")
 
-		self.cluster_labels = [0] * len(feature_vectors) # Replace this with actual computation
+
+		self.cluster_labels = [0] * len(feature_vectors) # label all cluster [0]
+
+		self.cluster_labels[0] = [0]	# label 1st cluster
+		self.aux_cluster = feature_vectors[1] # 1st aux cluster is 1st vector
+		self.sim_score = [0]
+		threshold_value = 0.8
+		for i in range(2,len(feature_vectors)): # do all vector from 2
+			eligible_cluster = 0
+			for j in range(len(self.aux_cluster)):	# do all current aux cluster
+				sim_cos = float(cosine_similarity( \
+				feature_vectors[i].reshape(1,-1),
+				self.aux_cluster[j].reshape(1,-1))) # check cosine
+				# sim_dist = abs(float(feature_vectors[i] - /
+				# self.aux_cluster[j])) # check distance
+				sim_dist = float(hypot(feature_vectors[i],
+				self.aux_cluster[j])) # check distance via hypot
+				self.sim_score[j] = np.mean(sim_cos, sim_dist) # similarity
+				if self.sim_score[j] > threshold_value: # check eligibility
+					eligible_cluster += 1
+					self.eligibility[j] = True
+
+			if eligible_cluster = 0: # create new cluster
+				self.cluster_labels[len(self.aux_cluster) + 1 ] = \
+				len(self.aux_cluster) + 1
+				self.aux_cluster[len(self.aux_cluster) + 1 ] = \
+				feature_vectors[i]
+			elif eligible_cluster = 1: # add vector to cluster
+				ok_cluster = self.eligibility.index(True)
+				self.aux_cluster[ok_cluster] = sum(self.aux_cluster,
+				feature_vectors[i]) / 2
+			else eligible_cluster > 1: # pick highest score, add to that cluster
+				 hi_sim = max(self.sim_score) # highest similarity
+
+
+
+
+
+
 		return self
+
+
+	def update_aux(self, aux_cluster):
+		pass
